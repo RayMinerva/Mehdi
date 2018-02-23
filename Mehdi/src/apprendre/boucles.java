2f05f1a3af8f3ea6fcad4298ccad89cd59b7f88b
RayMinerva <36516385+RayMinerva@users.noreply.github.com>
2018-02-23 18:54:32 +0100

    Add files via upload

---
+++ Mehdi/src/apprendre/boucles.java
@@ -0,0 +1,15 @@
+package apprendre;
+
+public class boucles {
+
+	public static void main(String[] args) {
+		int a = 1, b = 15;
+
+		while (a < b) {
+
+		   System.out.println("coucou " +a+ " fois !!");
+		   a++;
+	}
+
+}
+}
